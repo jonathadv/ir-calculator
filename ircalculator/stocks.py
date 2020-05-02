cb5e00d2854e7c12336c356c12f61744d7751b51
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2020-05-02 18:34:55 -0300

    Updates methods to use get_transactions()

--- ircalculator/stocks.py
+++ ircalculator/stocks.py
@@ -1,9 +1,9 @@
 from datetime import datetime
-from typing import List
+from typing import List, Union
 
 
 class Cost:
-    def __init__(self, name, value):
+    def __init__(self, name: str, value: str):
         self.name = name
         self.value = abs(float(value))
 
@@ -16,10 +16,16 @@ class Transaction:
     BUY = "BUY"
 
     def __init__(
-        self, ticker, optype, date, shares, price_per_share, costs: List[Cost]
+        self,
+        ticker: str,
+        trans_type: Union[SELL, BUY],
+        date: str,
+        shares: Union[str, int],
+        price_per_share: Union[str, float],
+        costs: List[Cost],
     ):
         self.ticker = ticker
-        self.optype = optype
+        self.trans_type = trans_type
         self.date = datetime.strptime(date, "%Y-%m-%d")
         self.shares = abs(int(float(shares.replace(",", "."))))
         self.price_per_share = abs(float(price_per_share.replace(",", ".")))
@@ -32,7 +38,7 @@ class Transaction:
     def total_with_cost(self):
         total_costs = self.total_cost()
 
-        if self.optype == self.SELL:
+        if self.trans_type == self.SELL:
             total_costs = -self.total_cost()
 
         return self.total() + total_costs
@@ -44,26 +50,29 @@ class Transaction:
         return (
             f"{self.__class__.__name__}("
             f"ticker={self.ticker}, "
-            f"type={self.optype}, "
+            f"type={self.trans_type}, "
             f"date={self.date}, "
             f"shares={self.shares}, "
             f"price_per_share={self.price_per_share})"
         )
 
     def __str__(self):
-        return f"[{self.date.strftime('%Y-%m-%d')}] | {self.optype} | {self.shares} x {self.price_per_share} = {self.total()}"
+        return (
+            f"[{self.date.strftime('%Y-%m-%d')}] | {self.trans_type} | "
+            f"{self.shares} x {self.price_per_share} = {self.total()}"
+        )
 
 
 class Stock:
-    def __init__(self, ticker):
+    def __init__(self, ticker: str):
         self.ticker = ticker
         self.purchased: List[Transaction] = []
         self.sold: List[Transaction] = []
 
     def add_operation(self, op: Transaction):
-        if op.optype == Transaction.BUY:
+        if op.trans_type == Transaction.BUY:
             self.purchased.append(op)
-        elif op.optype == Transaction.SELL:
+        elif op.trans_type == Transaction.SELL:
             self.sold.append(op)
         else:
             raise Exception(f"Unable to add Operariont {op}")
@@ -77,17 +86,37 @@ class Stock:
     def n_sold(self):
         return sum([i.shares for i in self.sold])
 
-    def total_amount(self):
-        transactions = self.sold + self.purchased
+    def total_amount(
+        self, transaction_type: Union[Transaction.SELL, Transaction.BUY] = None
+    ):
+        transactions = self.get_transactions(transaction_type)
         return sum([t.total() for t in transactions])
 
-    def total_amount_with_cost(self):
-        transactions = self.sold + self.purchased
+    def total_amount_with_cost(
+        self, transaction_type: Union[Transaction.SELL, Transaction.BUY] = None
+    ):
+        transactions = self.get_transactions(transaction_type)
         return sum([t.total_with_cost() for t in transactions])
 
-    def total_cost(self):
-        transactions: Transaction = self.sold + self.purchased
+    def total_cost(
+        self, transaction_type: Union[Transaction.SELL, Transaction.BUY] = None
+    ):
+        transactions = self.get_transactions(transaction_type)
         return sum([t.total_cost() for t in transactions])
+
+    def get_transactions(
+        self, transaction_type: Union[Transaction.SELL, Transaction.BUY] = None
+    ):
+        opts = {
+            Transaction.SELL: self.sold,
+            Transaction.BUY: self.purchased,
+        }
+
+        transactions: List[Transaction] = opts.get(
+            transaction_type, (self.sold + self.purchased)
+        )
+
+        return transactions
 
     def __repr__(self):
         return (
